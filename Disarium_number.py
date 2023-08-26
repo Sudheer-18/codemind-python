6f38e83d95f7a84eebebf0d6690268d2385ba99b
Sudheer-18 <119719131+Sudheer-18@users.noreply.github.com>
2023-08-26 14:25:29 +0530

    import math n=int(input()) t=n s=0 k=int(math.log10(n)+1) for i in range(k,0,-1):     r=n%10     s=s+int(r**i)     n//=10 if(s==t):     print("True") else:     print("False")

---
+++ Disarium_number.py
@@ -0,0 +1,14 @@
+import math
+n=int(input())
+t=n
+s=0
+k=int(math.log10(n)+1)
+for i in range(k,0,-1):
+    r=n%10
+    s=s+int(r**i)
+    n//=10
+if(s==t):
+    print("True")
+else:
+    print("False")
+    
