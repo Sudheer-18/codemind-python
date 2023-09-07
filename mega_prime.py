57d668c13d51f1aa848a2a53f2c97f9360ea5e30
Sudheer-18 <119719131+Sudheer-18@users.noreply.github.com>
2023-09-07 21:26:15 +0530

    def isprime(n):     if n==1:         return False     for i in range(2,n):         if n%i==0:             return False     return True import math n=int(input()) k=int(math.log10(n)+1) t=n c=0 while(n!=0):     r=n%10     n=n//10     if(isprime(r)):         c+=1 if(c==k):     if(isprime(t)):         print("Mega Prime")     else:         print("Not Mega Prime") else:     print("Not Mega Prime")

--- mega_prime.py
+++ mega_prime.py
@@ -1,25 +1,25 @@
 def isprime(n):
-    if n == 1:
+    if n==1:
         return False
     for i in range(2,n):
         if n%i==0:
             return False
     return True
 import math
-a=int(input())
-k=int(math.log10(a)+1)
-t=a
-flag=0
-while(a!=0):
-    r=a%10
-    a=a//10
+n=int(input())
+k=int(math.log10(n)+1)
+t=n
+c=0
+while(n!=0):
+    r=n%10
+    n=n//10
     if(isprime(r)):
-        flag+=1
-if(flag==k):
+        c+=1
+if(c==k):
     if(isprime(t)):
         print("Mega Prime")
     else:
         print("Not Mega Prime")
 else:
     print("Not Mega Prime")
-
+    
