c3bec44d7a496e0a3b6011895fe65fdf6eebfeff
Sudheer-18 <119719131+Sudheer-18@users.noreply.github.com>
2023-09-07 21:36:30 +0530

    n=int(input()) m=int(input()) c=0 p=0 for i in range(n,m+1):     c=0     if i!=1:         for j in range(2,int(i**0.5)+1):             if i%j==0:                 c=1                 break         if(c==0):             p+=1 print(p)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,14 +1,15 @@
-a=int(input())
-b=int(input())
+
+n=int(input())
+m=int(input())
 c=0
 p=0
-for i in range(a,b+1):
+for i in range(n,m+1):
     c=0
-    if i != 1:
+    if i!=1:
         for j in range(2,int(i**0.5)+1):
-            if(i%j==0):
+            if i%j==0:
                 c=1
-                break;
+                break
         if(c==0):
             p+=1
 print(p)
