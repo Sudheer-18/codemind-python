cd51b532ac546607dd7009fb1504bde3bdcbcd7e
Sudheer-18 <119719131+Sudheer-18@users.noreply.github.com>
2023-10-22 13:15:37 +0530

    def reverse_words(sentence):     # Split the sentence into words     words = sentence.split()
        # Reverse the list of words
        reversed_words = list(reversed(words))
        
        # Join the reversed words back into a sentence
        reversed_sentence = ' '.join(reversed_words)
        
        return reversed_sentence
    
    # Input sentence
    input_sentence = input()
    
    # Call the function to reverse the words
    result = reverse_words(input_sentence)
    
    # Print the reversed sentence
    print(result)

--- Reverse_String.py
+++ Reverse_String.py
@@ -1,2 +1,20 @@
-n=input()
-print(n[::-1])
+def reverse_words(sentence):
+    # Split the sentence into words
+    words = sentence.split()
+    
+    # Reverse the list of words
+    reversed_words = list(reversed(words))
+    
+    # Join the reversed words back into a sentence
+    reversed_sentence = ' '.join(reversed_words)
+    
+    return reversed_sentence
+
+# Input sentence
+input_sentence = input()
+
+# Call the function to reverse the words
+result = reverse_words(input_sentence)
+
+# Print the reversed sentence
+print(result)
