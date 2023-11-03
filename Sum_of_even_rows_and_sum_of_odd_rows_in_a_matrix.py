e4f954636f14b7b067a58be6e09861ed646e6982
Sudheer-18 <119719131+Sudheer-18@users.noreply.github.com>
2023-11-03 15:06:17 +0530

    r,c=map(int,input().split()) lst=[] for i in range(r):     lst.append(list(map(int,input().split()))[:c:]) e=o=0 for i in range(r):     for j in range(c):         if(i%2==0):             e+=lst[i][j]         else:             o+=lst[i][j] print(e,o)

---
+++ Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.py
@@ -0,0 +1,13 @@
+r,c=map(int,input().split())
+lst=[]
+for i in range(r):
+    lst.append(list(map(int,input().split()))[:c:])
+e=o=0
+for i in range(r):
+    for j in range(c):
+        if(i%2==0):
+            e+=lst[i][j]
+        else:
+            o+=lst[i][j]
+print(e,o)
+    
