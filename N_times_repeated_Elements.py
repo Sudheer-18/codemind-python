9670a3cde03bcfa97e4d32a07b7624c6b3a3a180
Sudheer-18 <119719131+Sudheer-18@users.noreply.github.com>
2023-11-11 20:34:00 +0530

    n=int(input()) arr=list(map(int,input().split())) k=int(input()) arr1=set(arr) arr1=list(arr1) el=[] for i in arr1:     if(arr.count(i)==k):         el.append(i) if len(el)==0:     print(-1) else:     print(*el)

--- N_times_repeated_Elements.py
+++ N_times_repeated_Elements.py
@@ -1,13 +1,13 @@
 n=int(input())
 arr=list(map(int,input().split()))
 k=int(input())
-lst=[]
 arr1=set(arr)
+arr1=list(arr1)
+el=[]
 for i in arr1:
-    if arr.count(i)==k:
-        lst.append(i)
-if len(lst)!=0:
-    print(*lst)
-else:
+    if(arr.count(i)==k):
+        el.append(i)
+if len(el)==0:
     print(-1)
-        
+else:
+    print(*el)
